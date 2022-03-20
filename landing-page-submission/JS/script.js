26650b5128ded4dfda92f696b08168051904a52b
Troy Delacruz <troy8076@yahoo.com>
2022-03-20 11:29:48 -0400

    Re-editing the JS

--- landing-page-submission/JS/script.js
+++ landing-page-submission/JS/script.js
@@ -1,62 +1,21 @@
-/* Define global variables */
+/* Defining global variable(s) */
 
-const sections = Array.from(document.querySelectorAll('section'));
-const menu = document.getElementById('navbar__list');
-let numberOfListItems = sections.length;
-/* End Global Variables */
+const navSections = querySelectorAll("section");
 
+/* Starting the beginning functions */
 
-/* Starting Functions */
+function createNavHTML(id, name) {
+  const navHTML = `<a class ="menu__link" data-id="${id}>${name}</a>`;
+  return navHTML;
+}
 
-function createListItem () {
-    for (section of sections) {
-        sectionName = section.getAttribute('data-nav');
-        sectionLink = section.getAttribute('id');
+function inViewport(elem) {
+  const bound = elem.getBoundingClientRect();
+  return bound.top >= 0;
+}
 
-        // Creates an item for each one
+/* Starting the functions that will build upon the previous */
 
-        listItem = document.createElement('li');
+// Building the navigation
 
-        // Adding item text
-
-        listItem.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${sectionName}</a>`;
-
-        // Adding list item to menu
-
-        menu.appendChild(listItem);
-    }
-};
-
-// Will show if section is in viewport
-
-function sectionInViewPort (elem) {
-    let sectionPos = elem.getBoundingClientRect();
-    return (sectionPos >= 0)
-};
-
-function toggleActiveClass() {
-    for (section of sections) {
-        
-        //If the section is in the viewport
-        if (sectionInViewPort(section)) {
-            //Check if it doesn't already have "your-active-class"
-            if (!section.classList.contains('your-active-class')) {
-                //Add it
-                section.classList.add('your-active-class');
-            }
-        } else { //Out of viewport? Remove your-active-class
-            section.classList.remove('your-active-class');
-        }
-    }
-};
-
-/*Completed helper functions */
-
-
-// Build the nav
-
-createListItem();
-
-// Add class 'active' to section when close to top of viewport
-
-document.addEventListener('scroll', toggleActiveClass);
+function buildNav() {}
